22f9339c4dee09315c249cb255d3399ffd7030a9
Takashi IIGUNI <takashi.iiguni@guni.dev>
2024-06-10 19:50:28 +0900

    setup port number and address
    Signed-off-by: Takashi IIGUNI <takashi.iiguni@guni.dev>

--- main.go
+++ main.go
@@ -9,11 +9,33 @@ import (
 )
 
 func main() {
+	var port uint
+	var address string
+
 	app := &cli.App{
 		Name:  "cnproxy",
 		Usage: "cloud native proxy",
+		Flags: []cli.Flag{
+			&cli.UintFlag{
+				Name:        "port",
+				Aliases:     []string{"p"},
+				Value:       8080,
+				Usage:       "port number",
+				Destination: &port,
+			},
+			&cli.StringFlag{
+				Name:        "address",
+				Aliases:     []string{"a"},
+				Value:       "0.0.0.0",
+				Usage:       "address",
+				Destination: &address,
+			},
+		},
 		Action: func(*cli.Context) error {
-			cnproxyServer := &server.CNProxyServer{}
+			cnproxyServer := &server.CNProxyServer{
+				Port:    port,
+				Address: address,
+			}
 			return cnproxyServer.Serve()
 		},
 	}
--- pkg/middleware/server/server.go
+++ pkg/middleware/server/server.go
@@ -1,19 +1,24 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/guni1192/cnproxy/pkg/middleware/logger"
 	"github.com/guni1192/cnproxy/pkg/service"
 )
 
-type CNProxyServer struct{}
+type CNProxyServer struct {
+	Port    uint
+	Address string
+}
 
 func (s *CNProxyServer) Serve() error {
 	h := &service.CNProxyHandler{
 		Logger: logger.New(),
 	}
 
-	h.Logger.Info("listen on :8080")
-	return http.ListenAndServe(":8080", h)
+	host := fmt.Sprintf("%s:%d", s.Address, s.Port)
+	h.Logger.Info("listening", "address", s.Address, "port", s.Port)
+	return http.ListenAndServe(host, h)
 }
